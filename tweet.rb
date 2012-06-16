0d7414918e87738dd32ea34d0a9d904846d76e84
soramugi <soramugi.chika@gmail.com>
2012-06-16 12:29:37 +0900

    アプリケーション削除した人の場合のエラー処理追加

--- tweet.rb
+++ tweet.rb
@@ -6,31 +6,33 @@ require 'twitter'
 
 DB = Sequel.connect('sqlite:'+ File.expand_path(File.dirname(__FILE__)) +'//midare.db')
 twitConsumer = DB[:twitConsumer]
-twitOauths = DB[:twitOauth].filter(:status_flag => 0)
-words = DB[:word].filter(:status_flag => 0)
+twitOauths   = DB[:twitOauth].filter(:status_flag => 0)
+words        = DB[:word].filter(:status_flag => 0)
 
 CONSUMER_KEY = twitConsumer.first[:key]
 CONSUMER_SECRET = twitConsumer.first[:secret]
 
 word = []
 words.each do |w|
-    word.push(w[:word])
+  word.push(w[:word])
 end
 
 twitOauths.each do |oauth|
 
-    Twitter.configure do |config|
-        config.consumer_key       = CONSUMER_KEY
-        config.consumer_secret    = CONSUMER_SECRET
-        config.oauth_token        = oauth[:toekn]
-        config.oauth_token_secret = oauth[:toekn_secret]
-    end
+  Twitter.configure do |config|
+    config.consumer_key       = CONSUMER_KEY
+    config.consumer_secret    = CONSUMER_SECRET
+    config.oauth_token        = oauth[:toekn]
+    config.oauth_token_secret = oauth[:toekn_secret]
+  end
 
-    if oauth[:name_id] != Twitter.user.screen_name then
-        twitOauths.filter(:id => oauth[:id]).update(:name_id => Twitter.user.screen_name)
-    end
+  Twitter.user rescue next
 
-    text = word[rand(word.length)] + "の乱れ #乱れ"
+  if oauth[:name_id] != Twitter.user.screen_name then
+    twitOauths.filter(:id => oauth[:id]).update(:name_id => Twitter.user.screen_name)
+  end
 
-    Twitter.update(text) rescue exit
+  text = word[rand(word.length)] + "の乱れ #乱れ"
+
+  Twitter.update(text) rescue next
 end
