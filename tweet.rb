fc77f318018ef49ed9c198bb2119ea90703a9baf
soramugi <soramugi.chika@gmail.com>
2012-08-05 14:16:29 +0900

    読みやすいように整頓

--- tweet.rb
+++ tweet.rb
@@ -5,17 +5,26 @@ require 'sequel'
 require 'twitter'
 
 file_path = File.expand_path(File.dirname(__FILE__))
-DB        = Sequel.connect('sqlite:'+ file_path +'//midare.db', :timeout => 1000)
+DB        = Sequel.connect(
+  'sqlite:'+ file_path +'//midare.db',
+  :timeout => 1000
+)
 
 twitOauths   = DB[:twitOauth].filter(:status_flag => 0)
-pit = Pit.get("twitter_midare", :require => {
-  "consumer_key"    => "consumer key",
-  "consumer_secret" => "consumer secret"
-})
+pit = Pit.get(
+  "twitter_midare",
+  :require => {
+    "consumer_key"    => "consumer key",
+    "consumer_secret" => "consumer secret"
+  }
+)
 
 CONSUMER_KEY    = pit['consumer_key']
 CONSUMER_SECRET = pit['consumer_secret']
-words           = open(file_path +'/word.txt', :encoding => Encoding::UTF_8).readlines
+words           = open(
+  file_path +'/word.txt',
+  :encoding => Encoding::UTF_8
+).readlines
 
 twitOauths.each do |oauth|
 
@@ -26,13 +35,21 @@ twitOauths.each do |oauth|
     config.oauth_token_secret = oauth[:toekn_secret]
   end
 
+  # 認証解除している人を確認,ユーザーネームが取ってこれるか
   Twitter.user rescue next
 
+  # TwitterIDを登録、前回からIDが変わっていないか
   if oauth[:name_id] != Twitter.user.screen_name then
-    twitOauths.filter(:id => oauth[:id]).update(:name_id => Twitter.user.screen_name)
+    twitOauths.filter(
+      :id => oauth[:id]
+    ).update(
+      :name_id => Twitter.user.screen_name
+    )
   end
 
+  # ツイートする単語を決定
   word = words.shuffle.first.chomp + "の乱れ #乱れ"
 
+  # ツイート
   Twitter.update(word) rescue next
 end
