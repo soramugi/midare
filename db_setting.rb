2e5961d9a52ff021596a0b405489b3fac54079b1
soramugi <soramugi.chika@gmail.com>
2013-01-04 02:12:50 +0900

    Processing is changed in thread

--- db_setting.rb
+++ db_setting.rb
@@ -1,55 +1,72 @@
 #!/usr/bin/ruby
 # encoding : utf-8
-require 'sequel'
-require 'pit'
-require 'twitter'
 
-file_path = File.expand_path(File.dirname(__FILE__))
-DB        = Sequel.connect(
-  'sqlite:'+ file_path +'//midare.db',
-  :timeout => 2000
-)
+class DbSet
+  require 'sequel'
+  require 'pit'
+  require 'twitter'
 
-users = DB[:user]
-pit = Pit.get(
-  "twitter_midare",
-  :require => {
-    "consumer_key"    => "consumer key",
-    "consumer_secret" => "consumer secret"
-  }
-)
-
-Twitter.configure do |config|
-  config.consumer_key    = pit['consumer_key']
-  config.consumer_secret = pit['consumer_secret']
-end
-
-users.each do |user|
-
-  @client = Twitter::Client.new(
-    :oauth_token        => user[:toekn],
-    :oauth_token_secret => user[:toekn_secret]
-  )
-
-  # 認証解除してないか確認
-  begin
-    @client.user
-  rescue
-    users.filter(:id => user[:id]).update(:status_flag => 1)
-    next
+  def initialize
+    path = File.expand_path(File.dirname(__FILE__))
+    db = Sequel.connect('sqlite:' + path + '//midare.db')
+    @users = db[:user]
+    @pit = Pit.get("twitter_midare", :require => {
+      "consumer_key"    => "consumer key",
+      "consumer_secret" => "consumer secret"
+    })
   end
 
-  # 認証確認、再有効
-  if user[:status_flag] == 1 then
-    users.filter(:id => user[:id]).update(:status_flag => 0)
+  def settings
+    thread_max = 10
+
+    jobqueue = Queue.new
+    @users.each do |client|
+      jobqueue.push(client)
+    end
+
+    threads = []
+    thread_max.times do
+      threads << Thread.start do
+        while !jobqueue.empty?
+          var = jobqueue.pop
+          setting(var)
+          sleep rand(0)
+        end
+      end
+    end
+    threads.each {|t| t.join}
   end
 
-  # 前回からTwitterIDが変わっていないか、変わっていたら登録し直し
-  if user[:twitter_id] != @client.user.screen_name then
-    users.filter(
-      :id => user[:id]
-    ).update(
-      :twitter_id => @client.user.screen_name
+  private
+
+  def setting(user)
+    Twitter.configure do |config|
+      config.consumer_key    = @pit['consumer_key']
+      config.consumer_secret = @pit['consumer_secret']
+    end
+
+    client = Twitter::Client.new(
+      :oauth_token        => user[:toekn],
+      :oauth_token_secret => user[:toekn_secret]
     )
+
+    begin
+      client.user
+    rescue
+      @users.filter(:id => user[:id]).update(:status_flag => 1)
+      return
+    end
+
+    if user[:status_flag] == 1 then
+      @users.filter(:id => user[:id]).update(:status_flag => 0)
+    end
+
+    if user[:twitter_id] != client.user.screen_name then
+      @users.filter(:id => user[:id])
+      .update(:twitter_id => client.user.screen_name)
+    end
   end
 end
+
+db_set = DbSet.new
+db_set.settings
--- tweet.rb
+++ tweet.rb
@@ -1,50 +1,57 @@
 #!/usr/bin/ruby
 # encoding : utf-8
-require 'pit'
-require 'sequel'
-require 'twitter'
+class Midare
+  require 'pit'
+  require 'sequel'
+  require 'twitter'
+  require 'thread'
 
-file_path = File.expand_path(File.dirname(__FILE__))
-DB        = Sequel.connect(
-  'sqlite:'+ file_path +'//midare.db',
-  :timeout => 2000
-)
+  def initialize
+    path = File.expand_path(File.dirname(__FILE__)).to_s
+    db = Sequel.connect('sqlite:' + path + '//midare.db')
 
-users = DB[:user].filter(:status_flag => 0)
-pit = Pit.get(
-  "twitter_midare",
-  :require => {
-    "consumer_key"    => "consumer key",
-    "consumer_secret" => "consumer secret"
-  }
-)
+    @users = db[:user].filter(:status_flag => 0)
+    @pit = Pit.get("twitter_midare", :require => {
+      "consumer_key"    => "consumer key",
+      "consumer_secret" => "consumer secret"
+    })
 
-words = open(
-  file_path +'/word.txt',
-  :encoding => Encoding::UTF_8
-).readlines
+    @words = open(path + '/word.txt', :encoding => Encoding::UTF_8).readlines
+  end
 
-Twitter.configure do |config|
-  config.consumer_key    = pit['consumer_key']
-  config.consumer_secret = pit['consumer_secret']
+  def thread_tweet
+    thread_max = 10
+
+    jobqueue = Queue.new
+    @users.each do |client|
+      jobqueue.push(client)
+    end
+
+    threads = []
+    thread_max.times do
+      threads << Thread.start do
+        while !jobqueue.empty?
+          var = jobqueue.pop
+          tweet(var)
+          sleep rand(0)
+        end
+      end
+    end
+    threads.each {|t| t.join}
+  end
+
+  def tweet(user)
+    Twitter.configure do |config|
+      config.consumer_key       = @pit['consumer_key']
+      config.consumer_secret    = @pit['consumer_secret']
+      config.oauth_token        = user[:toekn]
+      config.oauth_token_secret = user[:toekn_secret]
+    end
+
+    Twitter.update(@words.shuffle.first.chomp + "の乱れ #乱れ") rescue return
+  end
+
 end
 
-clients = []
-users.each do |user|
-  clients << Twitter::Client.new(
-    :oauth_token        => user[:toekn],
-    :oauth_token_secret => user[:toekn_secret]
-  )
-end
-
-clients.each do |client|
-
-  # 認証解除してないか確認
-  client.user rescue next
-
-  # ツイートする単語を決定
-  word = words.shuffle.first.chomp + "の乱れ #乱れ"
-
-  # ツイート
-  client.update(word) rescue next
-end
+midare = Midare.new
+midare.thread_tweet
